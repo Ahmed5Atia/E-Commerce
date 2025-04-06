605d742c6062a827a43ccb5c995a7aecb2d194ed
Ahmed5Atia <ahmed.atia.20092010@gmail.com>
2025-04-06 09:02:31 +0200

    fixed the navbar

--- pages/cart/navbar.js
+++ pages/cart/navbar.js
@@ -34,19 +34,19 @@ document.addEventListener("DOMContentLoaded", function () {
   `;
 
   document.getElementById("navbar-container").innerHTML = navbarHTML;
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
+  
+  navLinks.addEventListener("click", (e) => {
+    if (e.target.tagName === "A") {
+      hamburger.classList.remove("active");
+      navLinks.classList.remove("active");
+    }
+  });
 });
 
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navLinks.classList.toggle("active");
-});
-
-navLinks.addEventListener("click", (e) => {
-  if (e.target.tagName === "A") {
-    hamburger.classList.remove("active");
-    navLinks.classList.remove("active");
-  }
-});
